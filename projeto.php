d0de11635c0505c4b98bedd520a99f48c2768d0b
tiao <ssnkia@gmail.com>
2021-03-23 16:14:09 -0300

    Projeto PHP

---
+++ projeto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+    <style>
+
+    h2 {
+        color : #80a2ff;
+        text-shadow: 1px 1px 1px black;
+    }
+    </style>
+</head>
+<body>
+    <h1>TESTANDO PHP</h1>
+    <?php
+
+   echo  " <h2> Olá Mundoooo! </h2>" ;
+    ?>
+</body>
+</html>
