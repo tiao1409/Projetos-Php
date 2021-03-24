138c2edcd457222572473de1ca0a76793e0f5cca
tiao <ssnkia@gmail.com>
2021-03-24 00:41:30 -0300

    Atualização 24 de março de 2021

--- projeto.php
+++ projeto.php
@@ -2,6 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+  
     <title>PHP</title>
     <style>
 
@@ -12,10 +13,43 @@
     </style>
 </head>
 <body>
+<div> 
     <h1>TESTANDO PHP</h1>
+ 
     <?php
 
    echo  " <h2> Olá Mundoooo! </h2>" ;
+   /* Assuntos abaixo aula 4 ( lidos variaves e concatenções
+   
+   $n=4;
+    $nome= "Autor: Tião Silva Neto";
+    $idade= 30;
+
+    echo "$nome tem $idade anos!"; 
+
+    $n1 = 7;
+    $n2 = 2;
+    $n6 = 5.4;
+    $n3 = $n1 + $n2;
+    $n5 = 8000;
+    $n4 = ($n1 + $n2)/2;
+  
+ //Abaixo codigos de Aulas de Operadores Aritimeticos
+
+    echo "A SOMA ENTRE OS NUMEROS $n1 E $n2 É  $n3 ";
+        echo "<br/> A SOMA ENTRE OS NUMEROS ".($n1+$n2);
+        echo "<br/> A subtração dos numeros são: ".($n1-$n2);
+        echo "<br/> A multiplicação dos numeros são: ".($n1*$n2);
+        echo "<br/> A divisão dos numeros são: ".($n1/$n2);
+        echo "<br/> O modulo dos numeros são: ".($n1%$n2);
+        echo "<br/> A Média dos numeros são: ".$n4;
+        echo "<br/> O Valor absoluto dos numeros são: ".abs($n6);
+        echo "<br/> O valor do numero elevado $n1<sup>$n2</sup> =  ".pow($n1, $n2);
+        echo "<br/> A raiz quadrada do numero $n3 é = " . sqrt($n3);
+        echo "<br/> O valor arrendondado do numero é : ".round($n6); // ceil arredonda para cima floor arredonda para baixo
+        echo "<br/> A parte interia do numeros : ".intval($n6);
+        echo "<br/> O numero formatado em moedas e milhar: " .number_format($n5,2,",", ".");
+*/
     ?>
 </body>
 </html>
